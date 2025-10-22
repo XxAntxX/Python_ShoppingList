fc22cff26f1217652c315d995e3f8f6c86b06b9d
Ant <104728721+XxAntxX@users.noreply.github.com>
2025-10-22 11:10:28 +0200

    Update ShoppingList.py

--- ShoppingList.py
+++ ShoppingList.py
@@ -1,14 +1,24 @@
 #--------- Initiate variables
+list_name = null
+entries = []
 
-
+#---------- get shopping lists
 
 #---------- import file (shopping list)
 
 #---------- render shopping list in console
 
+#---------- input sanitizer and validator
+
+
 #---------- add new entry
 
+#---------- duplicate handling
 
 #---------- modify entry
 
 #---------- delete entry
+
+#---------- export list to file
+
+
